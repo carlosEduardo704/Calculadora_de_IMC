a00fea5cb887deca6f8a63f2be7e4e2ec9c470e5
Carlos Eduardo <97640110+carlosEduardo704@users.noreply.github.com>
2022-06-21 01:08:13 -0300

    montando o layout de design do projeto

--- design.py
+++ design.py
@@ -1,1 +1,23 @@
 import PySimpleGUI as psg
+
+layout = [
+    [psg.Text(), psg.InputText()],
+    [psg.Text(), psg.InputText()],
+    [psg.Text(), psg.InputText()],
+    [psg.Button()]
+]
+
+layout_calcular = [
+    [psg.Text(), psg.InputText()],
+    [psg.Text(), psg.InputText()],
+    [psg.Text(), psg.InputText()],
+    [psg.Button()]
+]
+
+janela_em_uso = psg.Window('CALCULADORA DE IMC', layout)
+
+while 1:
+    evento, valor = janela_em_uso.read()
+    if evento == psg.WIN_CLOSED:
+        break
+
